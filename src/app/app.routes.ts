7da610f3e598b164b5ad7de9687ce923e8dac30c
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-04-25 17:13:29 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProjectsComponent } from './projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: ProjectsComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'about', component: ProjectsComponent },
+    { path: 'contact', component: ProjectsComponent },
+    { path: '**', redirectTo: '/home' }
+];
