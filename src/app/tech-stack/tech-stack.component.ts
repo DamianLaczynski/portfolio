c9b8d65f5b855a46baadb812064531030b9a8f2c
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-04-26 16:47:18 +0200

    gh-pages deploy

---
+++ src/app/tech-stack/tech-stack.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../ui/header/header.component';
+
+@Component({
+  selector: 'app-tech-stack',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './tech-stack.component.html',
+  styleUrl: './tech-stack.component.css'
+})
+export class TechStackComponent {
+
+}
