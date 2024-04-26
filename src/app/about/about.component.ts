ecd894d8cc664407dfd20875559cde31b9e9de3b
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-04-26 22:01:22 +0200

    Update styles for mobile devices

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../ui/header/header.component';
+import { SectionComponent } from '../ui/section/section.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, SectionComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../ui/header/header.component';
+import { ProjectCardComponent } from '../ui/project-card/project-card.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, ProjectCardComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
