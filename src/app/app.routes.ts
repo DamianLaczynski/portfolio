27803a7f4636f73cffc71ef551c4ac20f430c1bd
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-04-27 18:36:15 +0200

    Add scrollbar style and some fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,9 +8,9 @@ import { AboutComponent } from './about/about.component';
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    /*{ path: 'about', component: AboutComponent},
+    { path: 'about', component: AboutComponent},
     { path: 'tech-stack', component: TechStackComponent },
     { path: 'projects', component: ProjectsComponent },
-    { path: 'contact', component: ContactComponent },*/
+    { path: 'contact', component: ContactComponent },
     { path: '**', redirectTo: '/home' }
 ];
