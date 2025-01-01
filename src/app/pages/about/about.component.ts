f4a33ffc40ae22fd3748d260e92a455c0be7db14
Damian Łączyński <damian.laczynski.edu@gmail.com>
2025-01-01 23:49:16 +0100

    refactor: restructure application components and update routing
    - Refactored the project structure by moving the About, Contact, Home, Projects, and Tech Stack components into a new 'pages' directory for better organization.
    - Updated import paths in various components to reflect the new structure, ensuring consistent access to shared components.
    - Removed the old AboutComponent and its associated files, replacing them with a new implementation in the 'pages' directory.
    - Added new shared components for Card, Header, Section, and Tag to enhance reusability across the application.
    - Updated the tsconfig.json to include path mappings for easier imports.
    - Cleaned up unused CSS and HTML files related to the old component structure, streamlining the codebase.

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '@shared/components/card/card.component';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { SectionComponent } from '@shared/components/section/section.component';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [HeaderComponent, SectionComponent, CardComponent],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}
