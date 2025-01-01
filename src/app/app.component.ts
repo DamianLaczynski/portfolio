190b45645a311ed5215fd74e129c85efcaa2ccfc
Damian Łączyński <damian.laczynski.edu@gmail.com>
2025-01-01 21:46:23 +0100

    chore: remove unused component test files and update app component imports
    - Deleted test files for AboutComponent, ContactComponent, HomeComponent, ProjectsComponent, and TechStackComponent to clean up the codebase.
    - Updated AppComponent to remove the TabbarComponent from the imports, streamlining the component structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './ui/navbar/navbar.component';
 import { FooterComponent } from './ui/footer/footer.component';
-import { TabbarComponent } from './ui/tabbar/tabbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, TabbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
