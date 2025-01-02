e0f80a19f46fe3ff1928a1838cdd6fdd6d3acaef
Damian Łączyński <damian.laczynski.edu@gmail.com>
2025-01-02 22:38:01 +0100

    feat: add fade animation to route transitions and update app structure
    - Implemented fade animation for route transitions in app.component.html and app.component.ts.
    - Updated app.component.ts to include a method for preparing the route outlet.
    - Modified app.config.ts to provide animations in the application configuration.
    - Created a new route-animations.ts file to define the fade animation.
    
    These changes enhance the user experience by providing smooth transitions between routes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { fadeAnimation } from './shared/animations/route-animations';
 
 @Component({
     selector: 'app-root',
     imports: [RouterOutlet, NavbarComponent, FooterComponent],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.css',
+    animations: [fadeAnimation]
 })
 export class AppComponent {
-
+    prepareRoute(outlet: RouterOutlet) {
+        return outlet && outlet.activatedRouteData;
+    }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withHashLocation())
+    provideRouter(routes),
+    provideAnimations()
   ]
 };
